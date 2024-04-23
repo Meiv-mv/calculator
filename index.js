95056689d5e8f70e804b165409aeeef0837a886a
Marco Verrone <programma.meiv@gmail.com>
2024-04-23 02:14:42 +0200

    Alpha Version - Base functioning

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 const backRowEl = document.getElementById("back-row-el")
 const mainRowEl = document.getElementById("main-row-el")
+const clearBtn = document.getElementById("clear-btn")
+let backRow = []
+
+function inputDisplay(i){
+  mainRowEl.value += i
+}
+
+function renderBackRow(a){
+  backRowEl.innerHTML = ""
+  backRow.push(mainRowEl.value)
+  backRow.push(a)
+  let buffer = ``
+  for (let i = 0; i < backRow.length; i++){
+    buffer += backRow[i] + " "
+  }
+  backRowEl.innerHTML = buffer
+  mainRowEl.value = ""
+}
+
+function result(){
+  renderBackRow("")
+  try {
+    mainRowEl.value = eval(backRowEl.innerHTML)
+  } catch (error){
+    mainRowEl.value = "Error"
+  }
+  backRow = []
+}
+
+clearBtn.addEventListener("click", function(){
+  backRowEl.innerHTML = ""
+  mainRowEl.value = ""
+  backRow = []
+})
